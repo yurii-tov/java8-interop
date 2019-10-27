e65991af32984de9afc5b8fac7f7a83efb659d42
Yurii <juretz32@yandex.ru>
2019-10-27 20:05:25 +0300

    Add ability to create Consumer without specifying parameter explicitly; Add docstrings

--- src/java8_interop/core.clj
+++ src/java8_interop/core.clj
@@ -1,18 +1,36 @@
 (ns java8-interop.core
   (:import (java.util.function Consumer Supplier)))
-        
+
 
 ;; Lambdas
 
 
-(defmacro consumer [param-vector & body]
-  (let [param (first param-vector)]
-    `(reify Consumer
-       (accept [this ~param]
-         (do ~@body)))))
+(defmacro consumer
+  "Create a new Consumer object.
+   Valid arguments are:
+    - Binding vector and body.
+      Example: (consumer [x]
+                 (do-somethin-with-x x)
+                 (do-other-thing x))
+    - Named function.
+      Example: (consumer println)"
+  [& args]
+  (let [[a1] args]
+    (if (vector? a1)
+      `(reify Consumer
+         (accept [this ~(first a1)]
+           (do ~@(rest args))))
+      `(consumer [~'x] (~a1 ~'x)))))
 
 
-(defmacro supplier [& body]
+(defmacro supplier
+  "Create a new Supplier object.
+   Valid arguments are:
+    - Arbitrary list of forms.
+      Example: (supplier (do-a)
+                         (do-b)
+                         (do-c))"
+  [& body]
   `(reify Supplier
      (get [this]
        (do ~@body))))
